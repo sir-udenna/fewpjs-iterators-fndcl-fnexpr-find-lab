23ab2d7de8739df2c6b3fd3446b0925c86ad66b8
sir-udenna <udennaboy@gmail.com>
2021-04-14 17:04:22 -0500

    commit tree f6e63912f6b3cc70f1954651df7dfce35025f55e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
 
+  function superbowlWin(array){
+      let yearWon = undefined;
+    array.find(element => {
+        if (element.result == "W"){
+            yearWon = element.year
+        }
+    })
+    return yearWon
+  }
